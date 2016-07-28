9bb646cea91470c12b8ef9a0fcf8804a4154d450
Pat Dayton <daytonpe14@gmail.com>
2016-07-28 09:37:29 -0500

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+#This page holds all our routes in a DSL (Domain Specific Language) 
+#tells Rails how to connect incoming requests to controllers and actions
+
+
+Rails.application.routes.draw do
+  
+  #Rails provides a resources method which can be used to declare a standard REST resource.
+	resources :articles
+
+	#get 'welcome/index' tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action
+  get 'welcome/index'
+
+  #root 'welcome#index' tells Rails to map requests to the root of the application to the welcome controller's index action
+  root 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
